359fe870cb243fb01ea6fd4b5a6e88841d31a7cf
Jovany Leandro G.C <bit4bit@riseup.net>
2016-01-23 16:15:13 -0500

    test concurrent connections to virtual socket

--- server_test.go
+++ server_test.go
@@ -5,11 +5,14 @@ import (
 	"crypto/tls"
 	"net/http"
 	"net/http/httptest"
+	"strings"
+	"sync"
 	"testing"
 )
 
 //TestDialAndListen test websocket tunnel
 func TestDialAndListen(t *testing.T) {
+	maxConns := 10
 	mux := http.NewServeMux()
 	mux.Handle("/remoton/", http.StripPrefix("/remoton", NewServer(
 		func(authToken string, r *http.Request) bool {
@@ -36,20 +39,46 @@ func TestDialAndListen(t *testing.T) {
 	go func() {
 		listener := session.Listen("test")
 
-		lconn, err := listener.Accept()
-		if err != nil {
-			t.Error(err)
-		}
-		data, _ := bufio.NewReader(lconn).ReadString('\n')
-
-		if data != "transfer" {
-			t.Errorf("want %v get %v", "transfer", data)
+		for i := 0; i < maxConns; i++ {
+			lconn, err := listener.Accept()
+			if err != nil {
+				t.Error(err)
+			}
+			data, _ := bufio.NewReader(lconn).ReadString('\n')
+			if strings.TrimSpace(data) != "transfer" {
+				t.Errorf("want %v get %v", "transfer", data)
+			} else {
+				lconn.Write([]byte("feedback"))
+				lconn.Write([]byte{'\n'})
+			}
 		}
 	}()
 
-	dconn, err := session.Dial("test")
-	dconn.Write([]byte("transfer"))
-	dconn.Close()
+	wg := &sync.WaitGroup{}
+	worker := func() {
+		go func() {
+			dconn, err := session.Dial("test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			dconn.Write([]byte("transfer"))
+			dconn.Write([]byte{'\n'})
+			data, err := bufio.NewReader(dconn).ReadString('\n')
+			if err != nil {
+				t.Error(err)
+			}
+			if strings.TrimSpace(data) != "feedback" {
+				t.Errorf("want %v get %v", "feedback", data)
+			}
+			dconn.Close()
+			wg.Done()
+		}()
+	}
+	for i := 0; i < maxConns; i++ {
+		wg.Add(1)
+		worker()
+	}
+	wg.Wait()
 }
 
 //TestDialAndListenTCP tcp tunnel
